1069c9c7a0fbca075706f406c5027b23f7f84ca1
khajamoon <119936035+khajamoon@users.noreply.github.com>
2024-05-12 08:07:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
-import  cors = from 'cors'
+import  cors  from 'cors'
 import user from './routes.js'
 
 dotenv.config()
